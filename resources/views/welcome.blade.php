3986df626a4a283f8acdfe1e4cf7107be8402156
Fabio Costa <fabiocosta.genova@gmail.com>
2023-05-05 20:42:28 +0200

    curata media query navbar logo e finita section con le 3 caselle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,39 +3,35 @@
     <x-header></x-header>
     <div class="container-fluid mt-5 pt-5">
         <div class="row justify-content-evenly px-3">
-            <div class="col-12 col-md-3 py-3">
-                <div class="pChiSiamo me-3  text-center">
-                    <i class="fa-solid fa-user fa-4x iconaCustom"></i>
+            <div class="col-12 col-lg-4 py-3 px-2">
+                <div class="iconSectionContainer me-3  text-center">
+                    <i class="fa-solid fa-circle-user fa-5x iconCustom"></i>
                 </div>
-                <h2 class="caselle">CHI SIAMO</h2>
+                <h2 class="caselle h5">CHI SIAMO</h2>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quasi dolorem unde laudantium illum corporis, commodi beatae, itaque veniam at ea recusandae culpa adipisci expedita ducimus ad ipsam sunt sapiente!</p>
                 <button class="bottoneCaselle btn rounded shadow fw-bold">Scopri di più</button>
             </div>
-            <div class="col-12 col-md-3 py-3">
-                <div class="pChiSiamo me-3  text-center">
-                    <i class="fa-solid fa-info fa-4x iconaCustom"></i>
+            <div class="col-12 col-lg-4 py-3 px-2">
+                <div class="iconSectionContainer me-3  text-center">
+                    <i class="fa-solid fa-circle-info fa-5x iconCustom"></i>
                 </div>
-                <h2 class="caselle ">INFORMAZIONI</h2>
+                <h2 class="caselle h5">INFORMAZIONI</h2>
                 <p>
                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quasi dolorem unde laudantium illum corporis, commodi beatae, itaque veniam at ea recusandae culpa adipisci expedita ducimus ad ipsam sunt sapiente!</p>
                 <button class="bottoneCaselle btn rounded shadow fw-bold">Scopri di più</button>
             </div>
-            <div class="col-12 col-md-3 py-3">
-                <div class="pChiSiamo me-3  text-center">
-                    <i class="fa-solid fa-user fa-4x iconaCustom"></i>
+            <div class="col-12 col-lg-4 py-3 px-2">
+                <div class="iconSectionContainer me-3  text-center">
+                    <i class="fa-solid fa-at fa-5x iconCustom"></i>
                 </div>
-                <h2 class="caselle">CONTATTI</h2>
+                <h2 class="caselle h5">CONTATTI</h2>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quasi dolorem unde laudantium illum corporis, commodi beatae, itaque veniam at ea recusandae culpa adipisci expedita ducimus ad ipsam sunt sapiente!</p>
                 <button class="bottoneCaselle btn rounded shadow fw-bold">Scopri di più</button>
             </div>
         </div>
     </div>
-    <section>
-        <h2>ciao</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore illum excepturi quam possimus porro necessitatibus magnam rerum inventore quisquam eius commodi, cupiditate labore quia, dignissimos qui non pariatur in veniam!</p>
-    </section>
 
 
 </x-layout>
