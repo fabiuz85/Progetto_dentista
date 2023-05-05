17f62ca8e1e64490bf21c8b40e7ed450747facf0
Fabio Costa <fabiocosta.genova@gmail.com>
2023-05-05 14:50:09 +0200

    sto finendo navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,10 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="https://kit.fontawesome.com/8237c8f03e.js" crossorigin="anonymous"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css" rel="stylesheet"/>
+    @vite(['resources/js/app.js','resources/css/app.css'])
+    
 </head>
 <body>
     <x-navbar/>
-    {{slot}}
-    <x-footer/>
+    {{$slot}}
+    {{-- <x-footer/> --}}
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"></script>
 </body>
 </html>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,48 @@
+<!-- Navbar -->
+<nav class="container-fluid navbar navbar-expand-lg bg-light navbar-light ">
+    <!-- Container wrapper -->
+    <div class="row w-100">
+        <div class="col-12 d-flex justify-content-end bg-danger w-100 gap-5">
+             
+                
+                    <a href="https://twitter.com/dottlucagiachin">
+                        <i class="fa fa-twitter fa-2xl"></i>
+                    </a>
+                
+                
+                    <a href="https://www.facebook.com/studiodentisticogiachin">
+                        <i class="fa fa-facebook fa-2xl"></i>
+                    </a>
+                
+                
+                    <a href="http://google.com/+studiodentisticogiachingenova">
+                        <i class="fa fa-google-plus fa-2xl"></i>
+                    </a>
+                
+            
+        </div>
+        <div class="col-12">  
+                <a class="navbar-brand" href="#"> <img class="img-fluid" src="/media/logo_home.png" alt="logo studio"></a>
+            <!-- Toggle button -->
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              MENU <i class="fas fa-bars"></i>
+            </button>
+        
+            <!-- Collapsible wrapper -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+                <!-- Link -->
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+        
+              </ul>
+        
+            </div>
+        </div>
+      <!-- Navbar brand -->
+    </div>
+    <!-- Container wrapper -->
+</nav>
+  <!-- Navbar -->
