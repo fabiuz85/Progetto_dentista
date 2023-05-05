ff8f2da1e2e048738252fd415911121d48ca60c5
Fabio Costa <fabiocosta.genova@gmail.com>
2023-05-05 17:03:23 +0200

    finita navbar graficamente

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,48 +1,57 @@
-<!-- Navbar -->
-<nav class="container-fluid navbar navbar-expand-lg bg-light navbar-light ">
-    <!-- Container wrapper -->
-    <div class="row w-100">
-        <div class="col-12 d-flex justify-content-end bg-danger w-100 gap-5">
-             
-                
-                    <a href="https://twitter.com/dottlucagiachin">
-                        <i class="fa fa-twitter fa-2xl"></i>
-                    </a>
-                
-                
-                    <a href="https://www.facebook.com/studiodentisticogiachin">
-                        <i class="fa fa-facebook fa-2xl"></i>
-                    </a>
-                
-                
-                    <a href="http://google.com/+studiodentisticogiachingenova">
-                        <i class="fa fa-google-plus fa-2xl"></i>
-                    </a>
-                
-            
-        </div>
-        <div class="col-12">  
-                <a class="navbar-brand" href="#"> <img class="img-fluid" src="/media/logo_home.png" alt="logo studio"></a>
-            <!-- Toggle button -->
-            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              MENU <i class="fas fa-bars"></i>
-            </button>
-        
-            <!-- Collapsible wrapper -->
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        
-                <!-- Link -->
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-        
-              </ul>
-        
-            </div>
-        </div>
-      <!-- Navbar brand -->
+
+<div class="fixed-top">
+
+    <div class="d-flex justify-content-end gap-3 w-100 px-4 pt-2 bg-transparent">
+        <a href="https://twitter.com/dottlucagiachin">
+            <i class="fa fa-twitter fa-xl navColor"></i>
+        </a>
+    
+    
+        <a href="https://www.facebook.com/studiodentisticogiachin">
+            <i class="fa fa-facebook fa-xl navColor"></i>
+        </a>
+    
+    
+        <a href="http://google.com/+studiodentisticogiachingenova">
+            <i class="fa fa-google-plus fa-xl navColor"></i>
+        </a>
     </div>
-    <!-- Container wrapper -->
-</nav>
-  <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg py-0">
+      <div class="container-fluid">
+          
+        <a class="navbar-brand" href="#"><img class="img-fluid" src="/media/logo_home.png" alt="logo studio"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          MENU
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 w-100 d-flex justify-content-evenly align-items-center py-5 my-0">
+            
+            <li class="nav-item p-3">
+              <a class="nav-link navColor" href="#"><i class="fa-solid fa-user-large"></i> CHI SIAMO</a>
+            </li>
+            <li class="nav-item p-3">
+                <a class="nav-link navColor" href="#"><i class="fa-regular fa-clipboard"></i> SERVIZI</a>
+            </li>
+            <li class="nav-item p-3">
+                <a class="nav-link navColor" href="#"><i class="fa-solid fa-user-group"></i> STAFF</a>
+            </li>
+            <li class="nav-item p-3">
+                <a class="nav-link navColor" href="#"><i class="fa-solid fa-rss"></i> NEWS</a>
+            </li>
+            <li class="nav-item p-3">
+                <a class="nav-link navColor" href="#"><i class="fa-solid fa-handshake-angle"></i> CONVENZIONI</a>
+            </li>
+            <li class="nav-item p-3">
+                <a class="nav-link navColor" href="#"><i class="fa-solid fa-location-dot"></i> CONTATTI</a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link rounded fw-bold shadow p-3 appointmentBtn h-100 w-100" href="#">  <p class="mb-0"><i class="fa-regular fa-calendar-days"></i> APPUNTAMENTI</p> </a>
+            </li>
+
+          </ul>
+          
+        </div>
+      </div>
+    </nav>
+</div>
